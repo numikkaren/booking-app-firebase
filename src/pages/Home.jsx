0190afa119eb1ea5615f035338004bdc7dc34c35
karen lee <karen@numik.co>
2025-03-31 00:41:14 +1300

    added Right and left panel on Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import EquipmentList from "../Components/EquipmentList.jsx";
+import Map from "../Components/Map.jsx";
 
 function Home({user}) {
   return (
-    <div>
-      <h1>Welcome to Equipment Rental Home Page</h1>
-      <p>Please see below for the equipment list for hire</p>
-      <EquipmentList user = {user} />
+    <div className="home-wrapper">            {/*  flex container */} 
+      <div className="home-left">        
+        <h1>Welcome to Equipment Rental Home Page</h1>
+
+        <p>Please see below for the equipment list for hire</p>
+        <EquipmentList user = {user} />
+      </div>
+      <div className="home-right">
+        <h2>Map Placeholder</h2>
+        <Map/>
+      </div>    
     </div>
   );
 }
